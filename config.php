1701957f1cf6b135e81658c04b33ab59479a1d1a
sbsatwork <97030650+sbsatwork@users.noreply.github.com>
2022-07-13 10:30:13 +0200

    Add files via upload
    Configuration of:
    - Telegram channels
    - Translation (DeepL key, target language)
    - Recipient email for reporting

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+
+############### Configuration area #######################
+
+// URLs of all channels that you would like to check
+$urlsOfTelegramChannels[] = "https://t.me/s/heise_de/";
+$urlsOfTelegramChannels[] = "https://t.me/s/netzpolitik_org/";
+
+$makeTranslation = false; // Boolean true or false
+$deepLAuthKey = ""; // your personal deepLAuthKey
+$deepLTargetLang = "de"; // Target language (e.g. "de" for German)
+$emailRecipient = ""; // The email address, where the report will be sent to
+
+############### Configuration area #######################
+
+?>
