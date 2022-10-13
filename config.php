364e6bbd78222ea649cc1e2f8cb09a7cf4ef5f93
sbsatwork <97030650+sbsatwork@users.noreply.github.com>
2022-10-13 14:43:39 +0200

    Add files via upload
    New config possibilites: 
    - Add specific keywords to trigger an alarm (e.g. your company name)
    - Customize the reporting emails header

--- config.php
+++ config.php
@@ -1,16 +1,26 @@
 <?php
-
 ############### Configuration area #######################
 
-// URLs of all channels that you would like to check
+// Array of all channels that should be checked
 $urlsOfTelegramChannels[] = "https://t.me/s/heise_de/";
 $urlsOfTelegramChannels[] = "https://t.me/s/netzpolitik_org/";
 
 $makeTranslation = false; // Boolean true or false
 $deepLAuthKey = ""; // your personal deepLAuthKey
 $deepLTargetLang = "de"; // Target language (e.g. "de" for German)
+
+// Email settings
 $emailRecipient = ""; // The email address, where the report will be sent to
+$emailPriority = "Normal";
+$emailHeader = 'From: MONITORING@YOURDOMAIN.COM' . "\r\n" .
+'Reply-To: YOURREPLY@YOURDOMAIN.COM' . "\r\n" .
+'X-Mailer: PHP/' . phpversion();
+
+// A list of keywords you are looking for in the telegram channels, e.g. your company name
+$keywords[] = "Cyber";
+
+$emailBody = "";
+$html = "";
 
 ############### Configuration area #######################
-
 ?>
